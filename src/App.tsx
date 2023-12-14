2fe26bcf934dd84964b37ac0ba0a9f69c8d9925a
Kapskii <kapskij20@gmail.com>
2023-12-14 16:55:20 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  axios({
+    method: 'get',
+    url: 'https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=b9bf822643abfab53681f5b9aaa37937&units=metric',
+  })
+    .then(function (response) {
+      console.log(response);
+      
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
